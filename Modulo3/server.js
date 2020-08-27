c7a621ce80d814ec1e344dc6cd7ec4a8e92ff50f
Alan Martins <alanhorcrux@gmail.com>
2020-08-26 21:01:27 -0300

    Passando dados via rotas

--- Modulo3/server.js
+++ Modulo3/server.js
@@ -9,11 +9,26 @@ server.use(express.static('public'));
 server.set("view engine","njk");
 
 nunjucks.configure("views",{
-    express:server 
+    express:server,
+    autoescape: false 
 })
 
 server.get("/", function(req,res){
-    return res.render("about");
+
+    const about = {
+        avartar_url: "https://avatars1.githubusercontent.com/u/20859616?s=460&v=4",
+        avartar_name: "Alan Martins",
+        nome: "Alan Vieira Martins",
+        job: "Engenheiro de Software",
+        description: "Desenvolvedor FullStack (.NET / ReactJS / NodeJS)",
+        links: [
+            {name: "GitHub", url: "https://github.com/alandlan"},
+            {name: "LinkedIn", url: "https://www.linkedin.com/in/alan-martins-38b35799/"}
+        ]
+
+    }
+
+    return res.render("about", {about: about});
 });
 
 server.get("/habilidades", function(req,res){
