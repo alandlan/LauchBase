76dc151e9996342a2a7308d5c19d53e758c895a7
Alan Martins <alanhorcrux@gmail.com>
2020-09-01 17:30:09 -0300

    Estilizando Header e Body

--- Modulo4/routes.js
+++ Modulo4/routes.js
@@ -2,7 +2,15 @@ const express = require('express');
 const routes = express.Router()
 
 routes.get('/',function(req,res){
-    return res.send('Ok');
+    return res.redirect('/languages');
+})
+
+routes.get('/languages',function(req,res){
+    return res.render('languages/index');
+})
+
+routes.get('/skills',function(req,res){
+    return res.send('skills');
 })
 
 module.exports = routes;
